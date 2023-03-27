e5947ee61f0699758ce08b53b9256d92f6ce38ec
Patrick Frantzen <patrick.frantzen@web.de>
2023-03-27 14:18:50 +0200

    update

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -58,7 +58,7 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     // const firebaseApp = initializeApp(environment.firebase);
     // this.db = getFirestore(firebaseApp);
-    //test
+    //test test
     this.route.params.subscribe(async (params) => {
       //When url is changed the Hero Data of this User is loaded
       this.gameId = params['id'];
